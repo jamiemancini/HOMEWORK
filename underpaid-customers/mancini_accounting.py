430f6531767d82bc50ffa7e1adcac274f32d6fb1
Jamie Mancini <jamiemancini@gmail.com>
2022-02-08 04:49:52 +0000

    defined the function get_incorrect_payments

--- underpaid-customers/mancini_accounting.py
+++ underpaid-customers/mancini_accounting.py
@@ -18,24 +18,29 @@
 #start by opening the file:
 the_file=open("customer-orders.txt")
 
-#create a for loop
-#loop through each line
-#line.rstrip() removes any blank spaces
-#line.rstrip() breaks up the line into a list using the '|' symbol
-for line in the_file:
-    line = line.rstrip()
-    line = line.split('|')
+#create a function called get_incorrect_payments
+#it takes in 1 argument 'text_file'
+def get_incorrect_payments(the_file)
 
-    #example: ['965', 'Irene Ward', '14', '1.85']
-    name = line[1]  #the name of the customer
-    melons = line[2] #the number of melons that the customer bought
-    paid = line[3] #the amount of money the customer paid
 
-    #We need to compare the actual cost to the amount paid by the customer
-    #each melon costs 1 dollar
+    #create a for loop
+    #loop through each line
+    #line.rstrip() removes any blank spaces
+    #line.rstrip() breaks up the line into a list using the '|' symbol
+    for line in the_file:
+        line = line.rstrip()
+        line = line.split('|')
 
-    if float(melons) != float(paid):
-        print(f'{name} paid ${paid} for {melons} melons ')
+        #example: ['965', 'Irene Ward', '14', '1.85']
+        name = line[1]  #the name of the customer
+        melons = line[2] #the number of melons that the customer bought
+        paid = line[3] #the amount of money the customer paid
+
+        #We need to compare the actual cost to the amount paid by the customer
+        #each melon costs 1 dollar
+
+        if float(melons) != float(paid):
+            print(f'{name} paid ${paid} for {melons} melons ')
 
 
 
